936cb6428ceb6ca3f3bdb6a9ec1d54f1179487cd
Iliass <tiiliass@gmail.com>
2016-02-11 09:33:29 -0800

    likely voters feature

--- Project_1/Iliass_project_1.R
+++ Project_1/Iliass_project_1.R
@@ -3,17 +3,18 @@ library(car)
 library(ggplot2)
 
 imageDirectory <- "~/Dropbox/Math567/Discovering-Statistics-Using-R/Project_1"
+start_date <- as.Date("2015-2-1")
 
 ## Republican party
 gop_chart <- pollstr_chart('2016-national-gop-primary')
-print(gop_chart)
-
+gop_estimate <- gop_chart[["estimates_by_date"]]
+gop_estimate <- gop_estimate[gop_estimate$Date >= start_date]
 ## plotting
-gop_plot <- (ggplot(chart1[["estimates_by_date"]], aes(x = date, y = value, color = choice))+ geom_line())
-
+gop_plot <- (ggplot(gop_estimate, aes(x = date, y = value, color = choice))+ geom_line())
+gop_plot
 ## Democrate party
 dem_chart <- pollstr_chart('2016-president-dem-primary', convert =TRUE)  
 print(dem_chart)
 
 ## plotting
-dem_plot <- (ggplot(chart1[["estimates_by_date"]], aes(x = date, y = value, color = choice))+ geom_line())
+dem_plot <- (ggplot(dem_chart[["estimates_by_date"]], aes(x = date, y = value, color = choice))+ geom_line())
--- Project_1/pollstR_test.R
+++ Project_1/pollstR_test.R
@@ -5,15 +5,20 @@ library(ggplot2)
 imageDirectory <- "~/Dropbox/Math567/Discovering-Statistics-Using-R/Project_1"
 
 chart1 <- pollstr_chart('2016-national-gop-primary')
-print(chart1)
+#print(chart1)
 
 ## plotting
 plot1 <- (ggplot(chart1[["estimates_by_date"]], aes(x = date, y = value, color = choice))+ geom_line())
-
+plot1
 ## Obama Favorable rating
 ## charts
+obama_favorable <- pollstr_chart('obama-favorable-rating')
+print(obama_favorable)
+
 (ggplot(obama_favorable[["estimates_by_date"]], aes(x = date, y = value, color = choice))+geom_line())
 
 ## polls
 obama_favorable_polls <- pollstr_polls(max_pages = 10000, chart = 'obama-favorable-rating', after = "2014-3-1")
-str(obama_favorable_polls)
+#str(obama_favorable_polls)
+
+
